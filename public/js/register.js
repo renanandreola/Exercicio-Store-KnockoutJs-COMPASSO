d20ccde52a873222e98ba15b7ef089ca9e6b19e2
Renan Marcos Andreolla <renan.andreolla_EST@pampa.compasso>
2020-02-21 16:21:04 -0300

    Validacao de do register com knockout

--- public/js/register.js
+++ public/js/register.js
@@ -1,4 +1,4 @@
-function send (event) {
+/*function send (event) {
     event.preventDefault();
 
 // busca valores na página HTML
@@ -110,19 +110,21 @@ else {
     password: password
 
   }
-
-  // ENVIA DADOS PARA O MONGODB
-  $.post('/register', data, function (res) {
-         if(res === 'ok') {
-           toastr["success"]("Cadastro realizado com sucesso!");
-           $('form').trigger('reset');
-         } else {
-           toastr["error"]("Erro: " + res);
-          }
- })
+*/
+/*
+// ENVIA DADOS PARA O MONGODB
+$.post('/register', data, function (res) {
+        if(res === 'ok') {
+          toastr["success"]("Cadastro realizado com sucesso!");
+          $('form').trigger('reset');
+        } else {
+          toastr["error"]("Erro: " + res);
+        }
+})
 }
+*/
 
-
+/*
 
 // LIMPA CAMPOS DO FORMULÁRIO
 function clear (){
@@ -284,6 +286,8 @@ $(document).ready(function() {
     //   })
     // })
     
+    */
+    
 
     // DATA BINDS - KNOCKOUT JS
 
@@ -294,19 +298,119 @@ $(document).ready(function() {
   //ko.applyBindings(new AppViewModel());
 
   //data-bind - text - show words
-function AppViewModel() {
-  this.userName = ko.observable("Digite seu nome");
-  this.userLastName = ko.observable("Digite seu sobrenome");
-  this.userEmail = ko.observable("Digite seu email");
-  this.userPhone = ko.observable("Digite seu fone")
-  var userPhone = this.userPhone
-  if(userPhone = number){
-    alert("ok")
+function formViewModel() {
+  this.userName = ko.observable("");
+
+  this.userLastName = ko.observable("");
+
+  this.userEmail = ko.observable("");
+
+  this.userPhone = ko.observable("");
+
+  this.userCep = ko.observable("");
+
+  this.userState = ko.observableArray(['Selecione', 
+                                       'Acre', 'Alagoas','Amapá','Amazonas','Bahia','Ceará',
+                                       'Distrito Federal','Espírito Santo','Goiás','Maranhão',
+                                       'Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará',
+                                       'Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro',
+                                       'Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima',
+                                       'Santa Catarina','São Paulo','Sergipe','Tocantins','Estrangeiro']);
+
+  this.userCity = ko.observable("");
+
+  this.userNeighborhood = ko.observable("");
+
+  this.userAddress = ko.observable("");
+
+  this.userNumber = ko.observable("");
+
+  this.userComplement = ko.observable("");
+
+  this.userPassword = ko.observable("");
+
+  this.userConfirmPassword = ko.observable("");
+
+  // alert de inicio de formulário
+  this.alert = ko.observable({show: true, msgAlert: "Preencha seus dados para se cadastrar", type: "info"})
+
+
+  // validação dos campos 
+this.submitForm = function(){
+  
+  if(this.userName() === ""){
+   this.alert({show: true, msgAlert: "Preencha seu nome", type: "danger"});
+  return  
+  }   
+
+  if(this.userLastName() === ""){
+    this.alert({show: true, msgAlert: "Preencha seu sobrenome", type: "danger"});
+  return
   }
+
+  if(this.userEmail() === ""){
+    this.alert({show: true, msgAlert: "Preencha seu email", type: "danger"});
+  return
+  }
+
+  if(this.userPhone() === ""){
+    this.alert({show: true, msgAlert: "Preencha seu telefone", type: "danger"});
+  return
+}
+
+  if(this.userCep() === ""){
+    this.alert({show: true, msgAlert: "Preencha seu cep", type: "danger"});
+  return
+  }
+
+  if(this.userState() === ""){
+    this.alert({show: true, msgAlert: "Preencha seu estado", type: "danger"});
+  return
+  }
+
+  if(this.userCity() === ""){
+    this.alert({show: true, msgAlert: "Preencha sua cidade", type: "danger"});
+  return
+  }
+
+  if(this.userNeighborhood() === ""){
+    this.alert({show: true, msgAlert: "Preencha seu bairro", type: "danger"});
+  return
+  }
+
+  if(this.userAddress() === ""){
+    this.alert({show: true, msgAlert: "Preencha seu endereço", type: "danger"});
+  }
+
+  if(this.userNumber() === ""){
+    this.alert({show: true, msgAlert: "Preencha o número de sua residência", type: "danger"});
+  return
+  }
+
+  if(this.userComplement() === ""){
+    this.alert({show: true, msgAlert: "Preencha o complemento", type: "danger"});
+  return
+  }
+
+  if(this.userPassword() === ""){
+    this.alert({show: true, msgAlert: "Preencha sua senha", type: "danger"});
+  return
+  }
+
+  if(this.userConfirmPassword() === ""){
+    this.alert({show: true, msgAlert: "Confirme sua senha", type: "danger"});
+  return
+  }
+
+  else{
+    this.alert({show: true, msgAlert: "Formulário de preenchido, falta o banco de dados kkk", type: "info"})
+  }
+ }
 }
 
 
 
 
+
   // Activates knockout.js
-  ko.applyBindings(new AppViewModel());
+  ko.applyBindings(new formViewModel());
